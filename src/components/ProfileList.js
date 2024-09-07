c89c6c167bdb7723ce7aff7d95faf4d573251100
ZohalMirkhel <zohalmirkhel@gmail.com>
2024-09-07 06:43:11 +0430

    changed the code

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -5,6 +5,62 @@ const ProfileList = () => {
   const joinedMissions = useSelector((state) => state.missions.joinedMissions);
 
   return (
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     <div className="container mx-auto p-4 flex justify-center md:justify-start space-x-4">
       <div className="w-full md:w-1/2 mt-4 md:mt-0">
         <table className="w-full table-auto border-separate border-spacing-0 border-2 border-black rounded-lg overflow-hidden">
